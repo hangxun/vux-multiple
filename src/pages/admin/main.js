28179173361318898f5ad09c60e08469c7a7b948
Refs <lhx1010339808@outlook.com>
2019-09-17 08:13:05 +0800

    初始化

---
+++ src/pages/admin/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+// import Home from './components/HelloFromVux'
+import Hello from '@/components/HelloWorld'
+
+Vue.use(VueRouter)
+
+const routes = [{
+  path: '/',
+  component: Hello
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
